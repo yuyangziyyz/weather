1ddd8594bf8e56e2511e6abaac75694ee6ffb5fc
yuyangziyyz <yuyangziyyz@gmail.com>
2021-02-19 23:52:02 +0900

    first

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+import App from './App.vue';
+import 'bootstrap/dist/css/bootstrap.min.css' ;
+import"./assets/animation.css";
+import"./assets/custom.css";
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
